82deecb5fbc76c421fdc1e7d43ba97e4aff3573f
oinejoy29-svg <oinejoy29@gmail.com>
2026-09-09 22:43:03 +0900

    Create home.js

---
+++ home/home.js
@@ -0,0 +1,26 @@
+/* =========================
+   HOME
+========================= */
+
+"use strict";
+
+
+/* =========================
+   HOME DATA
+========================= */
+
+let HOME_DATA = null;
+
+
+/* =========================
+   HOME INITIALIZE
+========================= */
+
+document.addEventListener("DOMContentLoaded", () => {
+  initHome();
+});
+
+
+async function initHome() {
+
+}
